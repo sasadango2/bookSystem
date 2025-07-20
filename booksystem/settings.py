98dc6d3d557e90823bc778afc6bcb071ea4da1eb
Kobayashi Sota <xxiangtai75@gmail.com>
2025-07-20 09:19:15 +0900

    Fix missing os import in settings.py

--- booksystem/settings.py
+++ booksystem/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,8 +80,6 @@ WSGI_APPLICATION = 'booksystem.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-import os
 
 DATABASES = {
     'default': {
